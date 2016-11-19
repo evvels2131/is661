15a6fd40b0e736d7238f30f16ad9a69283fba532
Tom <tgoralczyk1@gmail.com>
2016-11-19 14:35:23 -0500

    Completed and styled the 'home' screen

---
+++ src/key-path-scenario/app/js/main.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+  console.log('what the fuck');
+  const myApp = new Framework7();
+  const $$ = Dom7;
+
+  const mainView = myApp.addView('.view-main', {
+    dynamicNavbar: true
+  });
+})();
