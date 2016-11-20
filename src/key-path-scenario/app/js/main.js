00a58afb16c5fb80f9a7747ab59f06392c98775b
Tom <tgoralczyk1@gmail.com>
2016-11-19 20:33:50 -0500

    Minor changes

--- src/key-path-scenario/app/js/main.js
+++ src/key-path-scenario/app/js/main.js
@@ -6,4 +6,29 @@
   const mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
   });
+
+  // `Home` view
+  myApp.onPageInit('home', function (page) {
+    console.log('home');
+    addActive(0);
+  });
+
+  // `Settings` view
+  myApp.onPageInit('settings', function (page) {
+    console.log('settings');
+    addActive(3);
+  });
+
+  function addActive(id) {
+    const links = $$('.toolbar-inner').children();
+    const activeLink = links[id];
+
+    $$(links).each(function (idx, value) {
+      if ($$(value).hasClass('active')) {
+        $$(value).removeClass('active');
+      }
+    });
+
+    $$(activeLink).addClass('active');
+  }
 })();
